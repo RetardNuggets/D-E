819bf0299c8ca0c2b699dedd9574fc80d5b5f841
q <69131802+earthmite@users.noreply.github.com>
2022-01-29 18:24:06 -0800

    ssawdasdsad

--- public/login/login-page.js
+++ public/login/login-page.js
@@ -11,7 +11,7 @@ loginButton.addEventListener("click", (e) => {
   if (username === "SekritString" && password === "PleaseHelpMe") {
     alert("You have successfully logged in.");
     document.cookie += `CSRFToken=${CSRFToken}`;
-    window.location.replace("http://localhost:3000/encode");
+    window.location.replace("main.html");
   } else {
     loginErrorMsg.style.opacity = 1;
   }
