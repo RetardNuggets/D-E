868fa2062b9acc425328f4d1f0174d662213b96d
RetardNuggets <loganebrown2006@gmail.com>
2022-02-15 20:33:11 -0500

    Did some stuff (this probably belongs in programminghorror

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const path = require("path");
+const mainPath = path.resolve("./public/main/index.html");
 const loginPath = path.resolve("./public/login/login.html");
 const encodePath = path.resolve("./public/encode/encode.html");
 const decodePath = path.resolve("./public/decode/decode.html");
 
 const app = express();
 const port = process.env.PORT || 4756;
+
+app.get("/", (req, res) => {
+  res.sendFile(mainPath);
+});
 
 app.get("/login", (req, res) => {
   res.sendFile(loginPath);
@@ -19,11 +24,13 @@ app.get("/decode", (req, res) => {
   res.sendFile(decodePath);
 });
 
-app.get("/api/encode", (req, res) => {});
+app.use(express.static("public/main"));
 
 app.use(express.static("public/login"));
 
 app.use(express.static("public/encode"));
+
+app.use(express.static("public/decode"));
 
 app.listen(port, () => {
   console.log(`Running service at ${port}`);
